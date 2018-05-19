021effc5189fd3d629fc0c99e591b0baf368c460
Ming-Yu Liu <mingyu.liu.tw@gmail.com>
2018-05-18 20:35:38 -0700

    Fix PyTorch GIF bug

--- photo_gif.py
+++ photo_gif.py
@@ -9,8 +9,8 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 import numpy as np
-# import cv2
-# from cv2.ximgproc import guidedFilter
+import cv2
+from cv2.ximgproc import guidedFilter
 
 
 class GIFSmoothing(nn.Module):
@@ -19,55 +19,55 @@ class GIFSmoothing(nn.Module):
     self.r = r
     self.eps = eps
 
-  # def process(self, initImg, contentImg):
-  #   '''
-  #   :param initImg: intermediate output. Either image path or PIL Image
-  #   :param contentImg: content image output. Either path or PIL Image
-  #   :return: stylized output image. PIL Image
-  #   '''
-  #   if type(initImg) == str:
-  #     init_img = cv2.imread(initImg)
-  #     init_img = init_img[2:-2,2:-2,:]
-  #   else:
-  #     init_img = np.array(initImg)[:, :, ::-1].copy()
-  #
-  #   if type(contentImg) == str:
-  #     cont_img = cv2.imread(contentImg)
-  #   else:
-  #     cont_img = np.array(contentImg)[:, :, ::-1].copy()
-  #
-  #   output_img = guidedFilter(guide=cont_img, src=init_img, radius=self.r, eps=self.eps)
-  #   output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)
-  #   output_img = Image.fromarray(output_img)
-  #   return output_img
-
   def process(self, initImg, contentImg):
     '''
-    PYTORCH GIF
     :param initImg: intermediate output. Either image path or PIL Image
     :param contentImg: content image output. Either path or PIL Image
     :return: stylized output image. PIL Image
     '''
     if type(initImg) == str:
-      init_img = Image.open(initImg).convert('RGB')
-      init_img = transforms.ToTensor()(init_img).unsqueeze(0)
+      init_img = cv2.imread(initImg)
+      init_img = init_img[2:-2,2:-2,:]
     else:
-      init_img = transforms.ToTensor()(initImg).unsqueeze(0)
+      init_img = np.array(initImg)[:, :, ::-1].copy()
 
     if type(contentImg) == str:
-      cont_img = Image.open(contentImg).convert('RGB')
-      cont_img = transforms.ToTensor()(cont_img).unsqueeze(0)
+      cont_img = cv2.imread(contentImg)
     else:
-      cont_img = transforms.ToTensor()(contentImg).unsqueeze(0)
+      cont_img = np.array(contentImg)[:, :, ::-1].copy()
 
-    cont_img = cont_img.cuda()
-    init_img2 = init_img.cuda()
-    cont_img = Variable(cont_img, volatile=True)
-    init_img2 = Variable(init_img2, volatile=True)
-    output_img = GuidedFilter(r=self.r,eps=self.eps)(cont_img,init_img2)
-    output_img = torch.clamp(output_img,0,1)
-    output_img = transforms.ToPILImage()(output_img[0,:,:,:].data.cpu())
+    output_img = guidedFilter(guide=cont_img, src=init_img, radius=self.r, eps=self.eps)
+    output_img = cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)
+    output_img = Image.fromarray(output_img)
     return output_img
+
+  # def process(self, initImg, contentImg):
+  #   '''
+  #   PYTORCH GIF
+  #   :param initImg: intermediate output. Either image path or PIL Image
+  #   :param contentImg: content image output. Either path or PIL Image
+  #   :return: stylized output image. PIL Image
+  #   '''
+  #   if type(initImg) == str:
+  #     init_img = Image.open(initImg).convert('RGB')
+  #     init_img = transforms.ToTensor()(init_img).unsqueeze(0)
+  #   else:
+  #     init_img = transforms.ToTensor()(initImg).unsqueeze(0)
+  #
+  #   if type(contentImg) == str:
+  #     cont_img = Image.open(contentImg).convert('RGB')
+  #     cont_img = transforms.ToTensor()(cont_img).unsqueeze(0)
+  #   else:
+  #     cont_img = transforms.ToTensor()(contentImg).unsqueeze(0)
+  #
+  #   cont_img = cont_img.cuda()
+  #   init_img2 = init_img.cuda()
+  #   cont_img = Variable(cont_img, volatile=True)
+  #   init_img2 = Variable(init_img2, volatile=True)
+  #   output_img = GuidedFilter(r=self.r,eps=self.eps)(cont_img,init_img2)
+  #   output_img = torch.clamp(output_img,0,1)
+  #   output_img = transforms.ToPILImage()(output_img[0,:,:,:].data.cpu())
+  #   return output_img
 
 
 #
